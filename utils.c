052efbf55bee4a2806a0720754997785922c279a
Antonio <antoniomcfms@gmail.com>
2026-06-09 22:29:48 +0100

    feito?

--- utils.c
+++ utils.c
@@ -6,12 +6,16 @@ void removerQuebraLinha(char *str) {
     /*
         Remover '\n' do final da string, se existir.
     */
+    str [strcspn(str, "\n")] = '\0';
 }
 
 void limparBuffer(void) {
     /*
         Limpar caracteres restantes do stdin, se necessário.
     */
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+    }
 }
 
 int compararComando(const char *entrada, const char *comando) {
@@ -19,6 +23,6 @@ int compararComando(const char *entrada, const char *comando) {
         Verificar se a entrada começa com determinado comando.
         Pode ser feito com strncmp().
     */
-
+    return strncmp(entrada, comando, strlen(comando)) == 0;  
     return 0;
 }
